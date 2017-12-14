8192b076aa307d8ef35f2aa9e89ce42069deb7c9
XD0M3 <xd0m3@staff.eslgaming.com>
2017-12-14 21:37:35 +0100

    added more thing

--- index.js
+++ index.js
@@ -22,7 +22,6 @@
 //[2].children["0"].children[1]
         static getAnswers(){
           var answers = $(".esl-content").find("table");
-          console.log(answers);
           var ik = answers[2].children["0"].childElementCount;
           var an = [];
           for(var ion = 1; ion < ik; ion++){
@@ -70,6 +69,45 @@
             an[ion-1]=zwi;
           }
           return an;
+        }
+
+        static getLeague(){
+          var leage = [];
+          var creatorBracket = $('form[name="parentForm"]').find('table');
+          if(creatorBracket["0"].children["0"].children[3].childNodes[1].innerText == "League"){
+              var l = creatorBracket["0"].children["0"].children[3].children[1].innerHTML;
+              //<a href="/leagueoflegends/eu-nordic-east/lol/open/eune-5on5-fight-club-64/" target="_parent"><img src="https://cdn-eslgaming.akamaized.net/play/eslgfx/nodelogo/play_small.gif" width="16" height="12" border="0">&nbsp;LoL EU Nordic &amp; East 5on5 Fight Club #64</a>
+              var text = l.split('<a href="');
+              text = text[1];
+              text = text.split('');
+              return leage;
+          }
+        }
+
+        static getSquad(){
+          var gh = [];
+        var creatorBracket = $('form[name="parentForm"]').find('table');
+        if(creatorBracket["0"].children["0"].children[3].childNodes[1].innerText == "League"){
+          var squad = creatorBracket["0"].children["0"].children[4].children[1].innerHTML;
+          squad = squad.split("squad=");
+          squad = squad[1];
+          squad = squad.split('" target="_parent">');
+          gh[0] = squad[0];
+          squad = squad[1];
+          squad = squad.split("</a>");
+          squad = squad[0];
+          gh[1] = squad;
+        }
+        /*var squad = creatorBracket["0"].children["0"].childNodes[6].children[1].innerHTML;
+        squad = squad.split("squad=");
+        squad = squad[1];
+        squad = squad.split('" target="_parent">');
+        gh[0] = squad[0];
+        squad = squad[1];
+        squad = squad.split("</a>");
+        squad = squad[0];
+        gh[1] = squad;*/
+        return gh;
         }
 
         static getDates(){
@@ -154,10 +192,11 @@
 
 
     var css = ".table_esladdon td, .table_esladdon th {  background-color: white;  border: 1px solid #E6E6E6;  padding: 8px;  }  #btn-adminlist-toggle, #ticketbox-navbutton, .TextP, .TextN {  text-decoration: none;  color: #ffffff;  background: #1076e5;  border-color: ##0561C8;  padding: 1px 5px;  display: inline-block;  margin-bottom: 0;  white-space: nowrap;  border: 1px solid transparent;  border-radius: 3px;  user-select: none;  cursor: pointer;  text-align: center;  vertical-align: middle;  box-sizing: border-box;   font-size: 12px;  line-height: 1.5;  }  .TitleM {  font-size: 26px;  } .table_esladdon table {border-radius: 8px}";
-
-    if(Page.isAdminTickets()){
+var css2 = 'body{background: black; font-family: "Open Sans",sans-serif; font-size: 14px;}.main{width: 770px; background: #fff;}.TitleM{margin-top: 3px; font-size: 12pt; font-weight: 700; float: left; text-transform: uppercase; font-weight: bold;}#protest-panel{float: right;}.ticket{margin-left: 10px; margin-right: 10px; padding-top: 12px;}.ticket-info table{margin-top: 4px; margin-left: 10px;}.ticket-info{margin-top: 40px;}.ticket-info table td{width: 250px;}.ticketinfo-category{font-size: 12px;}.ticketinfo-display{font-size: 15px; font-weight: bold;}.protest-bottom-panel{float: right; margin-top: 12px; margin-right: 1px;}.ticket-title{margin-top: 55px; margin-left: 6px;}.ticket td{vertical-align: top;}.ticket-reply-blue{width: 740px; background: #c9ddff; border-radius: 5px; margin-left: 5px; min-height: 90px; margin-top: 10px;}.ticket-reply-red{width: 740px; background: #ffc9c9; border-radius: 5px; margin-left: 5px; min-height: 90px; margin-top: 10px;}.ticket-reply-normal{width: 740px; background: #E5E5E5; border-radius: 5px; margin-left: 5px; min-height: 90px; margin-top: 10px;}.ticket-comment{width: 740px; background: #ffe4c9; border-radius: 5px; margin-left: 5px; min-height: 90px; margin-top: 10px;}.ticket-footer{width: 200px; height: 100%; padding: 10px; line-height: 1.2em; float: left;}.ticket-footer .team-shortcut{color: #292929;}.ticket-footer .reply-id{color: #b7b7b7; font-size: 40px; margin-left: 6px; margin-top: 10px; margin-bottom: 10px;}.reply{padding-top: 10px; padding-bottom: 10px;}';
+      if(Page.isAdminTickets()){
       //Adding CSS
       Page.addCssStyle(css);
+        Page.addCssSytle(css2);
       //Adding Classes
       var table = $('div[class="esl-content"]').find('table');
       var tickets = 0;
@@ -192,12 +231,28 @@
     }
 
     if(Page.isTicket()){
+       Page.addCssStyle(css2);
        var id = InformationSupport.getSupportId();
        var creator = InformationSupport.getTicketCreator();
        var dates = InformationSupport.getDates();
        var admin = InformationSupport.getAdmin();
        var answers = InformationSupport.getAnswers();
-       console.log(answers);
+       var squad = InformationSupport.getSquad();
+       var league = InformationSupport.getLeague();
+
+       $('.esl-content').after('<!DOCTYPE html><html><head> <title>ESL Ticket Project</title> <meta charset="utf-8"> <link rel="stylesheet" href="https://cdn-eslgaming.akamaized.net/play/drupal/profiles/eslgaming_play/themes/eslgaming/eslgaming_play_base/dist/styles/eslgaming-play-base.styles.css?p0wc1" type="text/css"> <link rel="stylesheet" href="style.css" type="text/css"> </head><body> <div class="main"> <div class="ticket"> <div class="TitleM" id="id"> Protest Ticket #5553672 </div><div id="protest-panel"> <a class="btn btn-primary btn-xsmall">Back</a> <a class="btn btn-primary btn-xsmall">This protest is not to be handled (unlock)</a> </div><div class="ticket-info"> <table> <tr class="ticketinfo-category"> <td style="border-left: 5px solid #1076e5; padding-left: 3px;">Creator</td><td>Created on</td><td>Admin</td></tr><tr class="ticketinfo-display"> <td style="border-left: 5px solid #1076e5; padding-left: 3px;"><a id="creator">Pila Gigante</a> ( )</td><td id="createDate">08/12/17 18:37</td><td><a id="admin">Worst Ahri EUNE</a></td></tr><tr class="ticketinfo-category"> <td style="border-left: 5px solid red; padding-left: 3px; padding-top: 4px;">Against</td><td style="padding-top: 4px;">Changed on</td><td style="padding-top: 4px;">Status</td></tr><tr class="ticketinfo-display"> <td style="border-left: 5px solid red; padding-left: 3px;"><a>Nobody</a></td><td id="changeDate">13/12/17 23:45</td><td>Closed (<a>Reopen</a>)</td></tr><tr class="ticketinfo-category"> <td style="padding-top: 4px;">Match ID</td><td style="padding-top: 4px;">League</td><td style="padding-top: 4px;">Adminsquad</td></tr><tr class="ticketinfo-display"> <td><a id="Match">None</a></td><td><a id="league">LoL Open Ladder 1on1 EU West</a></td><td><a id="squad">League of Legends (Europe)</a></td></tr><tr class="ticketinfo-category"> <td style="padding-top: 4px;">Access Restriction</td></tr><tr class="ticketinfo-display"> <td>Restricted, squadmembers only</td></tr></table> <div class="protest-bottom-panel"> <a class="btn btn-primary btn-xsmall">Change restriction to none</a> <a class="btn btn-primary btn-xsmall">Change restriction to normal</a> <span>&nbsp&nbsp&nbsp</span> <a class="btn btn-primary btn-xsmall">Disable Comments</a> <span>&nbsp&nbsp&nbsp</span> <a class="btn btn-primary btn-xsmall">CheaterBO</a> </div><h2 class="ticket-title">he didnt show up in time</h2> </div><div class="ticket-reply-blue"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">RG </span> <span class="username"><strong><a>Pila Gigante</a></strong></span><br/> <span class="date">08/12/17 18:37</span><br/> <div class="reply-id">#1</div></div></td><td> <div class="reply"> he didnt show up in time. <br/> thanks </div></td></tr></table> </div><div class="ticket-reply-normal"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">ESL </span> <span class="username"><strong><a>Worst Ahri EUNE</a></strong></span><br/> <span class="date">08/12/17 21:08</span><br/> <div class="reply-id">#2</div></div></td><td> <div class="reply"> Hello Pila Gigante, <br/> hello EnoZ_, <br/> <br/> EnoZ_ has 48 hours to react to this protest. Should nothing happen within this period of time, the protest will be assessed according to the available facts. Later objections will not be taken into account. <br/> <br/> Best regards, <br/> Worst Ahri EUNE, ESL Admin </div></td></tr></table> </div><div class="ticket-reply-normal"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">ESL </span> <span class="username"><strong><a>Worst Ahri EUNE</a></strong></span><br/> <span class="date">10/12/17 23:42</span><br/> <div class="reply-id">#3</div></div></td><td> <div class="reply"> Hello Pila Gigante, <br/> hello EnoZ_, <br/> <br/> The match has been assessed according to the rules. <br/> <br/> Best regards, <br/> Worst Ahri EUNE, ESL Admin </div></td></tr></table> </div><div class="ticket-reply-red"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">y0! </span> <span class="username"><strong><a>EnoZ_</a></strong></span><br/> <span class="date">11/12/17 03:42</span><br/> <div class="reply-id">#4</div></div></td><td> <div class="reply"> Hhello , <br/> <br/> is that a joke ? the match wasnt even open !? the match time was not set to open guys ... <br/> <br/> Best regards, Enoz_ </div></td></tr></table> </div><div class="ticket-comment"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">ESL </span> <span class="username"><strong><a>Worst Ahri EUNE</a></strong></span><br/> <span class="date">12/12/17 20:00</span><br/> <div class="reply-id">#5</div></div></td><td> <div class="reply"> This is how an admin comment would look if it was here :) </div></td></tr></table> </div><div class="ticket-reply-normal"> <table> <tr> <td> <div class="ticket-footer"> <span class="team-shortcut">ESL </span> <span class="username"><strong><a>Worst Ahri EUNE</a></strong></span><br/> <span class="date">13/12/17 23:45</span><br/> <div class="reply-id">#6</div></div></td><td> <div class="reply"> Hello Pila Gigante, <br/> hello EnoZ_, <br/> <br/> The match was open, because you did accept the challenge. <br/> <br/> Best regards, <br/> Worst Ahri EUNE, ESL Admin <br/> </div></td></tr></table> </div></div></div></body></html>');
+
+      var href = window.location.href;
+      href = href.split("/support/" + id);
+      href = href[0];
+      href = href + "/admin_tickets/?squad=" + squad[0];
+
+        $('#id').text("Support Ticket #" + id);
+        $('#creator').text(creator[0]);
+        $('#admin').text(admin[0]);
+        $('#createDate').text(dates[0]);
+        $('changeDate').text(dates[1]);
+        $('#squad').text(squad[1]);
     }
 
 })();
